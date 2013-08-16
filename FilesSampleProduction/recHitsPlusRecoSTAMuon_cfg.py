095e694ba51179d8ab1ec32f3e11334f06d13862
Cesare <cesare.calabria23@gmail.com>
2013-08-16 16:34:44 +0200

    recHits + STA muons

---
+++ FilesSampleProduction/recHitsPlusRecoSTAMuon_cfg.py
@@ -0,0 +1,72 @@
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process("RecoSTAMuon")
+process.load("RecoMuon.Configuration.RecoMuon_cff")
+
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
+
+#process.Timing = cms.Service("Timing")
+process.options = cms.untracked.PSet( wantSummary = cms.untracked.bool(True) )
+
+# import of standard configurations
+process.load('Configuration.StandardSequences.Services_cff')
+process.load('SimGeneral.HepPDTESSource.pythiapdt_cfi')
+process.load('FWCore.MessageService.MessageLogger_cfi')
+process.load('Configuration.EventContent.EventContent_cff')
+process.load('SimGeneral.MixingModule.mixNoPU_cfi')
+process.load('Configuration.Geometry.GeometryExtended2019Reco_cff')
+process.load('Configuration.StandardSequences.MagneticField_38T_cff')
+process.load('Configuration.StandardSequences.Reconstruction_cff')
+process.load('Configuration.StandardSequences.EndOfProcess_cff')
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
+from Configuration.AlCa.GlobalTag import GlobalTag
+process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:upgrade2019', '')
+
+
+# Load GEM RecHit process
+#########################
+process.gemRecHits = cms.EDProducer("GEMRecHitProducer",
+                                        recAlgoConfig = cms.PSet(),
+                                        recAlgo = cms.string('GEMRecHitStandardAlgo'),
+                                        gemDigiLabel = cms.InputTag("simMuonGEMDigis"),
+                                        maskSource = cms.string('File'),
+                                        maskvecfile = cms.FileInPath('RecoLocalMuon/GEMRecHit/data/GEMMaskVec.dat'),
+                                        deadSource = cms.string('File'),
+                                        deadvecfile = cms.FileInPath('RecoLocalMuon/GEMRecHit/data/GEMDeadVec.dat')
+                                    )
+
+process.standAloneMuons.STATrajBuilderParameters.FilterParameters.EnableGEMMeasurement = cms.bool(True)
+process.standAloneMuons.STATrajBuilderParameters.BWFilterParameters.EnableGEMMeasurement = cms.bool(True)
+
+### Input and Output Files
+##########################
+process.source = cms.Source("PoolSource",
+                            fileNames = cms.untracked.vstring(
+    'file:step_GEM_DIGI.root'
+    )
+                            )
+
+process.output = cms.OutputModule("PoolOutputModule",
+                                      fileName = cms.untracked.string(
+    'file:step_GEM_LocalReco.root'
+    ),
+                                      outputCommands = cms.untracked.vstring(
+    'keep  *_*_*_*',
+    ),
+                                  SelectEvents = cms.untracked.PSet(
+    SelectEvents = cms.vstring('rechit_step')
+                )
+                                  )
+
+### Paths and Schedules
+#######################
+process.rechit_step  = cms.Path(process.gemRecHits*process.standAloneMuons)
+process.endjob_step  = cms.Path(process.endOfProcess)
+process.out_step     = cms.EndPath(process.output)
+
+
+process.schedule = cms.Schedule(
+        process.rechit_step,
+        process.endjob_step,
+        process.out_step
+        )
