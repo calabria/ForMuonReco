c4a7077e23a2f09e2fe267d3270fe22b665030fa
Cesare <cesare.calabria23@gmail.com>
2013-08-16 16:36:15 +0200

    new plots

--- GEMRecHitsReader/GEMRecHitsReader/src/GEMRecHitsReader.cc
+++ GEMRecHitsReader/GEMRecHitsReader/src/GEMRecHitsReader.cc
@@ -99,6 +99,9 @@
 #include "RecoMuon/TrackingTools/interface/MuonPatternRecoDumper.h"
 
 #include "SimDataFormats/Track/interface/SimTrackContainer.h"
+#include "CommonTools/UtilAlgos/interface/TFileService.h"
+#include "FWCore/ServiceRegistry/interface/Service.h"
+#include "DataFormats/Math/interface/deltaR.h"
 
 //
 // class declaration
@@ -124,6 +127,9 @@ class GEMRecHitsReader : public edm::EDAnalyzer {
 
       // ----------member data ---------------------------
 
+      std::map<std::string,TH1F*> histContainer_;
+      std::map<std::string,TH2F*> histContainer2D_; 
+
       edm::InputTag simHitLabel_;
       edm::InputTag recHitLabel_;
       edm::InputTag simTrackLabel_;
@@ -148,6 +154,8 @@ class GEMRecHitsReader : public edm::EDAnalyzer {
 // constructors and destructor
 //
 GEMRecHitsReader::GEMRecHitsReader(const edm::ParameterSet& iConfig):
+  histContainer_(),
+  histContainer2D_(),
   simHitLabel_(iConfig.getUntrackedParameter<edm::InputTag>("simHit")),
   recHitLabel_(iConfig.getUntrackedParameter<edm::InputTag>("recHit")),
   simTrackLabel_(iConfig.getUntrackedParameter<edm::InputTag>("simTrack")),
@@ -183,9 +191,6 @@ GEMRecHitsReader::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetu
   edm::ESHandle<MuonDetLayerGeometry> geo;
   iSetup.get<MuonRecoGeometryRecord>().get(geo);
 
-  edm::ESHandle<MagneticField> magfield;
-  iSetup.get<IdealMagneticFieldRecord>().get(magfield);
-
   edm::Handle<GEMRecHitCollection> gemRecHits; 
   iEvent.getByLabel("gemRecHits","",gemRecHits);
 
@@ -195,7 +200,7 @@ GEMRecHitsReader::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetu
   Handle<reco::TrackCollection> staTracks;
   iEvent.getByLabel(staTrackLabel_, staTracks);
 
-  std::cout<<"input "<<staTrackLabel_<<std::endl;
+  //std::cout<<"input "<<staTrackLabel_<<std::endl;
 
   ESHandle<MagneticField> theMGField;
   iSetup.get<IdealMagneticFieldRecord>().get(theMGField);
@@ -205,6 +210,12 @@ GEMRecHitsReader::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetu
 
   Handle<SimTrackContainer> simTracks;
   iEvent.getByLabel("g4SimHits",simTracks);
+
+  edm::Handle<edm::PSimHitContainer> GEMHits;
+  iEvent.getByLabel(edm::InputTag("g4SimHits","MuonGEMHits"), GEMHits);
+
+  edm::Handle<edm::PSimHitContainer> CSCHits;
+  iEvent.getByLabel(edm::InputTag("g4SimHits","MuonCSCHits"), CSCHits);
 
   for (GEMRecHitCollection::const_iterator recHit = gemRecHits->begin(); recHit != gemRecHits->end(); recHit++) {
 
@@ -237,54 +248,178 @@ GEMRecHitsReader::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetu
   }
 
 
-  std::cout<<"Num. GEMRechHits: "<<numGemRecHits<<" Num. CSCRecHits: "<<numCscRecHits<<std::endl; 
+  //std::cout<<"Num. GEMRechHits: "<<numGemRecHits<<" Num. CSCRecHits: "<<numCscRecHits<<std::endl; 
 
 
   SimTrackContainer::const_iterator simTrack;
   reco::TrackCollection::const_iterator staTrack;
+  GEMRecHitCollection::const_iterator recHit;
 
-  for (reco::TrackCollection::const_iterator staTrack = staTracks->begin(); staTrack != staTracks->end(); ++staTrack){
+  int numSimHitsPerEvt = 0;
+  int numRecHitsPerEvt = 0;
+  int numRecHitsPerEvtStaTrack = 0;
+  int numRecHitsOverlap = 0;
+  int numRecHitsNoOverlap = 0;
+  std::vector<GEMRecHit> matchedRecHits;
+  std::vector<GlobalPoint> matchedRecHitsGP;
+  std::vector<LocalPoint> matchedRecHitsLP;
+  std::vector<int> matchedRecHitsStrip;
 
-    	reco::TransientTrack track(*staTrack,&*theMGField,theTrackingGeometry); 
+  for (edm::PSimHitContainer::const_iterator itHit = GEMHits->begin(); itHit != GEMHits->end(); ++itHit){
 
-    	for(trackingRecHit_iterator recHit = staTrack->recHitsBegin(); recHit != staTrack->recHitsEnd(); ++recHit){
+	if(itHit->particleType() != 13) continue;
 
-      		const GeomDet* geomDet = theTrackingGeometry->idToDet((*recHit)->geographicalId());
-      		double r = geomDet->surface().position().perp();
-      		double z = geomDet->toGlobal((*recHit)->localPosition()).z();
-      		double x = geomDet->toGlobal((*recHit)->localPosition()).x();
-      		double y = geomDet->toGlobal((*recHit)->localPosition()).y();
-      		std::cout<<"x: "<<x<<" y: "<<y<<" r: "<< r <<" z: "<<z<<std::endl;
+	DetId id = DetId(itHit->detUnitId());
+	GEMDetId idGem = GEMDetId(itHit->detUnitId());
 
+	int region_sim = idGem.region();
+	int layer_sim = idGem.layer();
+        int chamber_sim = idGem.chamber();
+        //int roll_sim = idGem.roll();
 
-		if ((*recHit)->geographicalId().det() == DetId::Muon){
+	histContainer_["hSimHitChamber"]->Fill(chamber_sim);
 
-			if ((*recHit)->geographicalId().subdetId() == MuonSubdetId::CSC){
+	GlobalPoint pointSimHit = theTrackingGeometry->idToDetUnit(id)->toGlobal(itHit->localPosition());
 
-				std::cout<<"CSC id: "<<CSCDetId((*recHit)->geographicalId().rawId())<<std::endl;
-				numCscRecHitsFromSTA++;
+	//float x_sim = itHit->localPosition().x();
+	float simX = pointSimHit.x();
+	float simY = pointSimHit.y();
+	float simPhi = pointSimHit.phi();
+	float simEta = pointSimHit.eta();
+	//int stripNum_sim = itHit->firstClusterStrip();
 
-			}
+	histContainer2D_["hSimHitOcc"]->Fill(simX, simY);
 
-		}
+	numSimHitsPerEvt++;
 
-		if ((*recHit)->geographicalId().det() == DetId::Muon){
+  	for (recHit = gemRecHits->begin(); recHit != gemRecHits->end(); recHit++) {
 
-			if ((*recHit)->geographicalId().subdetId() == MuonSubdetId::GEM){
+		GEMDetId rollId = (GEMDetId)(*recHit).gemId();
+		LocalPoint recHitPos=recHit->localPosition();
+		const GEMEtaPartition* rollasociated = gemGeom->etaPartition(rollId);
+		const BoundPlane & GEMSurface = rollasociated->surface(); 
+		GlobalPoint GEMGlobalPoint = GEMSurface.toGlobal(recHitPos);
 
-				std::cout<<"GEM id: "<<GEMDetId((*recHit)->geographicalId().rawId())<<std::endl;
-				numGemRecHitsFromSTA++;
+		int region = rollId.region();
+		int layer = rollId.layer();
+		int chamber = rollId.chamber();
+      		//int roll = rollId.roll();
 
-			}
+		double x = GEMGlobalPoint.x();
+		double y = GEMGlobalPoint.y();
+		//double z = GEMGlobalPoint.z();
 
-		}
+		double recPhi = GEMGlobalPoint.phi();
+		double recEta = GEMGlobalPoint.eta();
+		double dR = deltaR(simEta, simPhi, recEta, recPhi);
+
+		histContainer_["hDR"]->Fill(dR);
+
+		if(!(region_sim == region && layer_sim == layer)) continue;
+		if(dR > 0.1) continue;
+
+	 	histContainer_["hRecHitChamber"]->Fill(chamber);
+		histContainer2D_["hRecHitOcc"]->Fill(x, y);
+		numRecHitsPerEvt++;
+
+		int stripNum = recHit->firstClusterStrip();
+		//double x_reco = recHitPos.x();
+		//float phi_02pi = recPhi < 0 ? recPhi + TMath::Pi() : recPhi;
+		//float phiDeg = phi_02pi * 180/ TMath::Pi();
+
+		//if(phiDeg > 5 && phiDeg < 15){
+
+			//std::cout<<"Local x: "<<x_reco<<std::endl;
+			//std::cout<<"Global x: "<<x<<" y: "<<y<<" z: "<<z<<std::endl;
+			//std::cout<<"StripNumber: "<<stripNum<<std::endl;
+			matchedRecHits.push_back(*recHit);
+			matchedRecHitsGP.push_back(GEMGlobalPoint);
+			matchedRecHitsLP.push_back(recHitPos);
+			matchedRecHitsStrip.push_back(stripNum);
+
+		//}
 
 
     	}
 
+   	for (staTrack = staTracks->begin(); staTrack != staTracks->end(); ++staTrack){//Inizio del loop sulle STA track
+
+		for(trackingRecHit_iterator recHit = staTrack->recHitsBegin(); recHit != staTrack->recHitsEnd(); ++recHit){
+
+			if (!((*recHit)->geographicalId().det() == DetId::Muon)) continue;
+			if (!((*recHit)->geographicalId().subdetId() == MuonSubdetId::GEM)) continue;
+
+			GEMDetId id((*recHit)->geographicalId());
+			const GeomDet* geomDet = theTrackingGeometry->idToDet((*recHit)->geographicalId());
+
+			int region = id.region();
+			int layer = id.layer();
+			int chamber = id.chamber();
+	      		//int roll = id.roll();
+
+			double x = geomDet->toGlobal((*recHit)->localPosition()).x();
+			double y = geomDet->toGlobal((*recHit)->localPosition()).y();
+			double z = geomDet->toGlobal((*recHit)->localPosition()).z();
+			GlobalPoint GEMGlobalPoint = GlobalPoint(x,y,z);
+
+			double recPhi = GEMGlobalPoint.phi();
+			double recEta = GEMGlobalPoint.eta();
+			double dR = deltaR(simEta, simPhi, recEta, recPhi);
+
+			histContainer_["hDRStaTrack"]->Fill(dR);
+
+			if(!(region_sim == region && layer_sim == layer)) continue;
+			if(dR > 0.1) continue;
+
+		 	histContainer_["hRecHitChamberStaTrack"]->Fill(chamber);
+			histContainer2D_["hRecHitOccStaTrack"]->Fill(x, y);
+			numRecHitsPerEvtStaTrack++;
+
+			//int stripNum = recHit->firstClusterStrip();
+			//double x_reco = (*recHit)->localPosition().x();
+			//float phi_02pi = recPhi < 0 ? recPhi + TMath::Pi() : recPhi;
+			//float phiDeg = phi_02pi * 180/ TMath::Pi();
+
+			//if(phiDeg > 5 && phiDeg < 15){
+
+				//std::cout<<"Local x: "<<x_reco<<std::endl;
+				//std::cout<<"Global x: "<<x<<" y: "<<y<<" z: "<<z<<std::endl;
+				//std::cout<<"StripNumber: "<<stripNum<<std::endl;
+
+				for(int i = 0; i < (int)matchedRecHitsGP.size(); i++){
+
+					GlobalPoint gpTemp = matchedRecHitsGP[i];
+
+					double xTemp = gpTemp.x();
+					double yTemp = gpTemp.y();
+					double zTemp = gpTemp.z();
+
+					if((x-xTemp) == 0 && (y-yTemp) == 0 && (z-zTemp) == 0) numRecHitsOverlap++;
+					else{
+
+						numRecHitsNoOverlap++;
+						histContainer2D_["hMissingRecHitsLocPos"]->Fill(matchedRecHitsLP[i].x(), matchedRecHitsStrip[i]);
+
+					}
+
+				}
+
+			//}
+
+		}
+
+	}
+
+
   }
 
-  std::cout<<"Num. GEMRechHitsSTA: "<<numGemRecHitsFromSTA<<" Num. CSCRecHitsSTA: "<<numCscRecHitsFromSTA<<std::endl; 
+  histContainer_["hSimHitMult"]->Fill(numSimHitsPerEvt);
+  histContainer_["hRecHitMult"]->Fill(numRecHitsPerEvt);
+  histContainer_["hRecHitMultStaTrack"]->Fill(numRecHitsPerEvtStaTrack);
+  histContainer_["hRecHitMultOverlap"]->Fill(numRecHitsOverlap);
+  histContainer_["hRecHitMultNoOverlap"]->Fill(numRecHitsNoOverlap);
+
+  //std::cout<<"Num. GEMRechHitsSTA: "<<numGemRecHitsFromSTA<<" Num. CSCRecHitsSTA: "<<numCscRecHitsFromSTA<<std::endl; 
 
 }
 
@@ -293,6 +428,27 @@ GEMRecHitsReader::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetu
 void 
 GEMRecHitsReader::beginJob()
 {
+
+  edm::Service<TFileService> fs;
+  histContainer_["hSimHitChamber"] = fs->make<TH1F>("SimHitChamber", "SimHitChamber", 36, 0, 36);
+  histContainer_["hRecHitChamber"] = fs->make<TH1F>("RecHitChamber", "RecHitChamber", 36, 0, 36);
+  histContainer_["hRecHitChamberStaTrack"] = fs->make<TH1F>("RecHitChamberStaTrack", "RecHitChamberStaTrack", 36, 0, 36);
+
+  histContainer_["hSimHitMult"] = fs->make<TH1F>("SimHitMult", "SimHitMult", 5, 0, 5);
+  histContainer_["hRecHitMult"] = fs->make<TH1F>("RecHitMult", "RecHitMult", 5, 0, 5);
+  histContainer_["hRecHitMultStaTrack"] = fs->make<TH1F>("RecHitMultStaTrack", "RecHitMultStaTrack", 5, 0, 5);
+  histContainer_["hRecHitMultOverlap"] = fs->make<TH1F>("RecHitMultOverlap", "RecHitMultOverlap", 5, 0, 5);
+  histContainer_["hRecHitMultNoOverlap"] = fs->make<TH1F>("RecHitMultNoOverlap", "RecHitMultNoOverlap", 5, 0, 5);
+
+  histContainer_["hDR"] = fs->make<TH1F>("DR", "DR", 300, 0, 1);
+  histContainer_["hDRStaTrack"] = fs->make<TH1F>("DRStaTrack", "DRStaTrack", 300, 0, 1);
+
+  histContainer2D_["hSimHitOcc"] = fs->make<TH2F>("SimHitOcc", "SimHitOcc", 100, -260, 260, 100, -260, 260);
+  histContainer2D_["hRecHitOcc"] = fs->make<TH2F>("RecHitOcc", "RecHitOcc", 100, -260, 260, 100, -260, 260);
+  histContainer2D_["hRecHitOccStaTrack"] = fs->make<TH2F>("RecHitOccStaTrack", "RecHitOccStaTrack", 100, -260, 260, 100, -260, 260);
+
+  histContainer2D_["hMissingRecHitsLocPos"] = fs->make<TH2F>("RecHitOccStaTrackLocPos", "RecHitOccStaTrackLocPos", 100, -50, 50, 400, 0, 400);
+
 }
 
 // ------------ method called once each job just after ending the event loop  ------------
